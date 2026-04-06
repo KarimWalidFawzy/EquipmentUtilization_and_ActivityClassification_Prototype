c6f09f9bc0c68d350a3e6ff683724939d3ed4295
KarimWalidFawzy <kwasaq.0183@gmail.com>
2026-04-06 19:57:43 +0200

    ==========================

---
+++ app/app.py
@@ -0,0 +1,5 @@
+#Use apache kafka
+#Use OpenCV
+#Get video clips of construction equipment, track utilization states, differentiating between active and inactive states 
+# Classify specific work activities and calculate the time spent working vs idling 
+# Then stream results to a message broker to a simple UI
